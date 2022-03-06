b287bdc50030846be1d0cf1e989643bff355d15f
Igor Felix <igor.felix@loggi.com>
2022-03-06 19:02:11 -0300

    Add postgres compose container

--- clientes/views.py
+++ clientes/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def list_clientes(request):
+    return HttpResponse("Lista de clientes: 001, 002")
--- cryptobank/urls.py
+++ cryptobank/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('clientes/', include('clientes.urls')),
+    path('clientes/index', include('clientes.urls')),
+    path('clientes/list', include('clientes.urls')),
     path('admin/', admin.site.urls),
 ]
